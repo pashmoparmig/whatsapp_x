a0d9bc89d00e73ab49d42b7339cfc3882bd36410
pashmoparmig <107766161+pashmoparmig@users.noreply.github.com>
2022-06-24 16:13:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("khjh");
+/*
+
+num = document.getElementById('num');
+massage = document.getElementById('massage');
+
+function openlink(){
+window.open('https://wa.me/send?text='+ massage.value + '&phone=2' + num.value);
+}
+*/
